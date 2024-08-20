a1b58a13b2b4db15f7f0e391dc451093e3ab6953
AsedK <99304188+AsedK@users.noreply.github.com>
2024-08-19 22:21:59 -0700

    idk man pt2

--- src/components/ExperienceSection.js
+++ src/components/ExperienceSection.js
@@ -49,7 +49,7 @@ const ExperienceSection = () => {
     >
       <h2>My Experience</h2>
       <div className="experience-cards">
-        {/* New Genbai.ai Experience */}
+        {/* New Genbai.ai Experience testing*/}
         <div className="experience-card">
           <div className="card-header">
             <img
